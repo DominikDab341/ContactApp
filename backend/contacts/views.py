d9209fc44111ad4a2dc78324dff48a6ae017af97
Dominik Dąbrowski <281053@student.pwr.edu.pl>
2026-01-16 12:43:34 +0100

    feat: add CRUD endpoints for contact model

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters, permissions
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+class ContactViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ContactSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["last_name", "created_at"]
+    ordering = ["last_name"]
+
+    def get_queryset(self):
+        return Contact.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
