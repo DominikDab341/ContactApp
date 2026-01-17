b9423cbd4bdeb156ae291329eb96c6bdc9b0a7d5
Dominik Dąbrowski <281053@student.pwr.edu.pl>
2026-01-17 23:29:27 +0100

    feat: implement csv file upload

--- backend/contacts/serializers.py
+++ backend/contacts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Contact
+from phonenumber_field.serializerfields import PhoneNumberField
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,3 +8,11 @@ class ContactSerializer(serializers.ModelSerializer):
         fields = ['id', 'first_name', 'last_name', 'phone_number', 'email', 'town', 'status',
                   'created_at']
         read_only_fields = ['id','created_at']
+
+class CsvImportSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length=64)
+    last_name = serializers.CharField(max_length=128)
+    phone_number = PhoneNumberField()
+    email = serializers.EmailField()
+    town = serializers.CharField(max_length=256, required=False, allow_blank=True)
+    
--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from rest_framework import viewsets, filters, permissions
-from .models import Contact
-from .serializers import ContactSerializer
+from django.db import transaction
+from rest_framework import viewsets, filters, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Contact, ContactStatusChoices
+from .serializers import ContactSerializer, CsvImportSerializer
+import csv
+import io
 
 class ContactViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
@@ -15,3 +20,56 @@ class ContactViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=False, methods=['POST'], url_path='upload-contacts')
+    def upload_contacts(self, request):
+        file = request.FILES.get('file')
+        if not file:
+            return Response({"detail": "File is required."}, status=status.HTTP_400_BAD_REQUEST)
+        if not file.name.endswith('.csv'):
+            return Response({"detail": "Please upload a CSV file."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            decoded_file = file.read().decode('utf-8-sig')
+            reader = csv.DictReader(io.StringIO(decoded_file))
+            new_contacts = []
+            errors = []
+
+            new_status_obj = ContactStatusChoices.objects.filter(status='nowy').first()
+            if not new_status_obj:
+                return Response(
+                    {"detail": "'Nowy' status not found in database."}, 
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+            
+            for index, row in enumerate(reader, start=1):
+                serializer = CsvImportSerializer(data=row)
+                
+                if serializer.is_valid():
+                    instance = Contact(**serializer.validated_data)
+                    instance.status_id = new_status_obj.id
+                    instance.owner = request.user
+                    new_contacts.append(instance)
+                else:
+                    errors.append({
+                        "row": index,
+                        "errors": serializer.errors
+                    })
+
+            if new_contacts:
+                    Contact.objects.bulk_create(
+                    new_contacts, 
+                    ignore_conflicts=True
+                )
+
+            return Response({
+                "status": "ok",
+                "errors": errors
+            })
+        
+        except UnicodeDecodeError:
+            return Response({"detail": "Invalid file encoding"}, status=status.HTTP_400_BAD_REQUEST)
+        except csv.Error as e:
+            return Response({"detail": f"CSV parsing error: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
