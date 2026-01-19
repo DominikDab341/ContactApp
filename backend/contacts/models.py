4a065cfe37e592619fcdb23b18eb550f4126de30
JaroDomcio <281053@student.pwr.edu.pl>
2026-01-19 22:42:06 +0100

    fix(backend): different users can have same email and phone in his contacts

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -12,11 +12,23 @@ class Contact(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=128)
-    phone_number = PhoneNumberField(unique=True)
-    email = models.EmailField(unique=True)
+    phone_number = PhoneNumberField()
+    email = models.EmailField()
     town = models.CharField(max_length=256)
     status = models.ForeignKey(ContactStatusChoices, on_delete=models.PROTECT)
     created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['owner', 'phone_number'],
+                name='unique_owner_phone_number'
+            ),
+            models.UniqueConstraint(
+                fields=['owner', 'email'],
+                name='unique_owner_email'
+            )
+        ]
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
